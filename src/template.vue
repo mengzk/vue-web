0f3901cd25f8683e874dd8e49dcfd64f64d60045
meng <meng.liu1@bthome.com>
2024-04-18 09:23:18 +0800

    添加说明

--- src/template.vue
+++ src/template.vue
@@ -46,16 +46,7 @@ nav a.router-link-exact-active:hover {
   background-color: transparent;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
+/* 宽度超过 1024 的布局 */
 @media (min-width: 1024px) {
   header {
     display: flex;
@@ -65,21 +56,6 @@ nav a:first-of-type {
 
   .logo {
     margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
   }
 }
 </style>
