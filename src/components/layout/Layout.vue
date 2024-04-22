ac83f0f3e4fc254cb947a276697614c50a07a3f3
meng <meng.liu1@bthome.com>
2024-04-22 22:38:09 +0800

    asd

---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <section class="container">
+    <router-view>
+      <template #default="{ Component, route }">
+        <keep-alive>
+          <component :is="Component" :key="route.fullPath" />
+        </keep-alive>
+      </template>
+    </router-view>
+    <Debug></Debug>
+  </section>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+</style>
