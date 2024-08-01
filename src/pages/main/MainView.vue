1fbb3eee80c7d8e98ae067d274080163fe05e91a
meng <meng.liu1@bthome.com>
2024-08-01 21:56:01 +0800

    修改代码

--- src/pages/main/MainView.vue
+++ src/pages/main/MainView.vue
@@ -6,15 +6,23 @@
       </keep-alive>
     </router-view>
     <div class="row tab-layout">
-      <router-link class="tab-link" to="/home">Login</router-link>
-      <router-link class="tab-link" to="/hot">Hot</router-link>
-      <router-link class="tab-link" to="/my">My</router-link>
+      <router-link class="column center tab-link" to="/home" @click="onTabClick">
+        <img class="tab-link-icon" />
+        <span>Home</span>
+      </router-link>
+      <router-link class="column center tab-link" to="/hot" @click="onTabClick">
+        <img class="tab-link-icon" />
+        <span>Hot</span>
+      </router-link>
+      <router-link class="column center tab-link" to="/my" @click="onTabClick">
+        <img class="tab-link-icon" />
+        <span>My</span>
+      </router-link>
     </div>
   </div>
 </template>
 
 <script setup>
-
 /**
  * https://cn.vuejs.org/guide/built-ins/keep-alive.html
  * 当一个组件实例从 DOM 上移除但因为被 <KeepAlive> 缓存而仍作为组件树的一部分时，它将变为不活跃状态而不是被卸载。当一个组件实例作为缓存树的一部分插入到 DOM 中时，它将重新被激活。
@@ -32,6 +40,11 @@
     })
  * 
  */
+
+
+function onTabClick() {
+  console.log('onTabClick');
+}
 </script>
 
 <style scoped>
@@ -47,21 +60,24 @@
   flex-direction: column;
 }
 .tab-layout {
-  height: 56px;
-  border-top: solid 1px #e0e0e0;
+  height: 50px;
+  border-top: solid 1px #e6e6e6;
   align-items: center;
   background-color: white;
 }
 .tab-link {
   flex: 1;
-  padding: 12px 0;
-
   color: #000;
-  font-size: 20px;
+  font-size: 14px;
   font-weight: 500;
   text-align: center;
   text-decoration: none;
-
-  cursor: pointer;
+  /* cursor: pointer; */
+}
+.tab-link-icon {
+  width: 24px;
+  height: 24px;
+  margin-bottom: 4px;
+  background-color: coral;
 }
 </style>
