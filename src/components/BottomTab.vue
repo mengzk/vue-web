c42e472c481b1648aa48f854bcbdf1e26f8e8ab6
meng <meng.liu1@bthome.com>
2024-08-02 11:44:03 +0800

    修改代码

---
+++ src/components/BottomTab.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="tab-layout">
+    <router-link class="tab-link" to="/home" @click="onItemTap">
+      <img class="tab-link-icon" />
+      <span>Home</span>
+    </router-link>
+    <router-link class="tab-link" to="/hot" @click="onItemTap">
+      <img class="tab-link-icon" />
+      <span>Hot</span>
+    </router-link>
+    <router-link class="tab-link" to="/my" @click="onItemTap">
+      <img class="tab-link-icon" />
+      <span>My</span>
+    </router-link>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  tabs: {
+    type: Array,
+    required: false,
+    value: () => [],
+  },
+  onChangeTab: {
+    type: Function,
+    required: false,
+  },
+});
+
+function onItemTap(e) {
+  console.log('onItemTap', e);
+}
+</script>
+
+<style scoped>
+.tab-layout {
+  height: 50px;
+  display: flex;
+  align-items: center;
+  border-top: solid 1px #e6e6e6;
+  background-color: white;
+}
+.tab-link {
+  flex: 1;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  color: #000;
+  font-size: 14px;
+  font-weight: 500;
+  text-align: center;
+  text-decoration: none;
+  /* cursor: pointer; */
+}
+.tab-link:hover {
+  color: coral;
+}
+.tab-link-icon {
+  width: 24px;
+  height: 24px;
+  margin-bottom: 4px;
+  background-color: #c3c3c3;
+}
+.tab-link-icon:hover {
+  background-color: coral;
+}
+</style>
--- src/pages/main/MainView.vue
+++ src/pages/main/MainView.vue
@@ -5,24 +5,12 @@
         <component :is="Component" />
       </keep-alive>
     </router-view>
-    <div class="row tab-layout">
-      <router-link class="column center tab-link" to="/home" @click="onTabClick">
-        <img class="tab-link-icon" />
-        <span>Home</span>
-      </router-link>
-      <router-link class="column center tab-link" to="/hot" @click="onTabClick">
-        <img class="tab-link-icon" />
-        <span>Hot</span>
-      </router-link>
-      <router-link class="column center tab-link" to="/my" @click="onTabClick">
-        <img class="tab-link-icon" />
-        <span>My</span>
-      </router-link>
-    </div>
+    <BottomTab />
   </div>
 </template>
 
 <script setup>
+import BottomTab from '@/components/BottomTab.vue';
 /**
  * https://cn.vuejs.org/guide/built-ins/keep-alive.html
  * 当一个组件实例从 DOM 上移除但因为被 <KeepAlive> 缓存而仍作为组件树的一部分时，它将变为不活跃状态而不是被卸载。当一个组件实例作为缓存树的一部分插入到 DOM 中时，它将重新被激活。
@@ -58,26 +46,5 @@ function onTabClick() {
   flex: 1;
   display: flex;
   flex-direction: column;
-}
-.tab-layout {
-  height: 50px;
-  border-top: solid 1px #e6e6e6;
-  align-items: center;
-  background-color: white;
-}
-.tab-link {
-  flex: 1;
-  color: #000;
-  font-size: 14px;
-  font-weight: 500;
-  text-align: center;
-  text-decoration: none;
-  /* cursor: pointer; */
-}
-.tab-link-icon {
-  width: 24px;
-  height: 24px;
-  margin-bottom: 4px;
-  background-color: coral;
 }
 </style>
