022cc0e7296e23e1ece013e70f7abe28c8f63260
meng <meng.liu1@bthome.com>
2024-09-04 10:03:44 +0800

    修改代码

---
+++ src/components/debug/DebugTag.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="v-debug-tag" @click="onClick">
+    <div class="v-debug-tag-str">Debug</div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  env: {
+    type: String,
+    required: false,
+    default: 'dev',
+  },
+});
+
+function onClick(e) {
+}
+
+function onLongClick(e) {
+}
+</script>
+
+<style scoped>
+.v-debug-tag {
+  width: 72px;
+  height: 24px;
+  top: 0;
+  right: 0;
+  z-index: 99999;
+  position: fixed;
+  transform: rotate(45deg) translate(10, -10);
+  background-color: rgba(255,0,0,0.8);
+}
+.v-debug-tag-str {
+  align-items: center;
+  color: white;
+  font-size: 14px;
+  font-weight: 700;
+}
+</style>
